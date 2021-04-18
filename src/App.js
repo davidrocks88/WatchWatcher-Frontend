df91920730ab03ee5f51e47ed0c4108d951f757f
davidrocks88 <D1a0v9i4dX1e0n9o4>
2021-04-18 14:23:31 -0400

    auth and uploading images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 
-// import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import 'firebase/storage';  // <----
+
 import {useState, useEffect} from "react";
 import Clock from 'react-clock';
 import 'react-clock/dist/Clock.css';
 import WebcamCapture from "./components/WebcamCapture";
+import {FirebaseAuthConsumer, FirebaseAuthProvider, IfFirebaseAuthed, IfFirebaseAuthedAnd} from "@react-firebase/auth";
 
 require('dotenv').config()
 
-
+const config = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
+}
 
 
 // const firebaseApp = firebase.initializeApp({
@@ -26,20 +37,20 @@ require('dotenv').config()
 
 function App() {
 
-  // const click = () => {
+    // const click = () => {
     // Add a new document in collection "cities"
-  //   db.collection("cities").doc("LA").set({
-  //     name: "Los Angeles",
-  //     state: "CA",
-  //     country: "USA"
-  //   })
-  //       .then(function() {
-  //         console.log("Document successfully written!");
-  //       })
-  //       .catch(function(error) {
-  //         console.error("Error writing document: ", error);
-  //       });
-  // }
+    //   db.collection("cities").doc("LA").set({
+    //     name: "Los Angeles",
+    //     state: "CA",
+    //     country: "USA"
+    //   })
+    //       .then(function() {
+    //         console.log("Document successfully written!");
+    //       })
+    //       .catch(function(error) {
+    //         console.error("Error writing document: ", error);
+    //       });
+    // }
 
     const [currentTime, setValue] = useState(new Date());
     const [showWebcam, setShowWebcam] = useState(false)
@@ -58,16 +69,68 @@ function App() {
     }, []);
 
 
-  return (
-    <div className="App">
-        <header className="App-header">Watch Watcher</header>
-        <Clock value={currentTime} />
-        <p>{`${currentTime.toDateString()} ${currentTime.toTimeString()} ${currentTime.getMilliseconds()}`}</p>
+    return (
+        <FirebaseAuthProvider firebase={firebase} {...config}>
+            <div>
+                <button
+                    onClick={() => {
+                        const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                        firebase.auth().signInWithPopup(googleAuthProvider);
+                    }}
+                >
+                    Sign In with Google
+                </button>
+                <button
+                    data-testid="signin-anon"
+                    onClick={() => {
+                        firebase.auth().signInAnonymously();
+                    }}
+                >
+                    Sign In Anonymously
+                </button>
+                <button
+                    onClick={() => {
+                        firebase.auth().signOut();
+                    }}
+                >
+                    Sign Out
+                </button>
+                <FirebaseAuthConsumer>
+                    {({ isSignedIn, user, providerId }) => {
+                        return (
+                            <pre style={{ height: 300, overflow: "auto" }}>
+                {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+              </pre>
+                        );
+                    }}
+                </FirebaseAuthConsumer>
+                <div>
+                    <IfFirebaseAuthed>
+                        {() => {
+                            return <div>You are authenticated</div>;
+                        }}
+                    </IfFirebaseAuthed>
+                    <IfFirebaseAuthedAnd
+                        filter={({ providerId }) => providerId !== "anonymous"}
+                    >
+                        {({ providerId }) => {
+                            return <div>You are authenticated with {providerId}</div>;
+                        }}
+                    </IfFirebaseAuthedAnd>
+                </div>
+            </div>
+            <div className="App">
+                <header className="App-header">Watch Watcher</header>
+                <Clock value={currentTime}/>
+                <p>{`${currentTime.toDateString()} ${currentTime.toTimeString()} ${currentTime.getMilliseconds()}`}</p>
 
-        <button onClick={() => setShowWebcam(!showWebcam)}>Record Time</button>
-        {showWebcam && <WebcamCapture getCurrentTime={()=>currentTime} stopWebcam={()=>setShowWebcam(false)} />}
-    </div>
-  );
+                <button onClick={() => setShowWebcam(!showWebcam)}>Record Time</button>
+                {showWebcam &&
+                <WebcamCapture firebase={firebase} getCurrentTime={() => currentTime} stopWebcam={() => setShowWebcam(false)}/>}
+            </div>
+        </FirebaseAuthProvider>
+
+    );
 }
 
 export default App;
--- src/components/WebcamCapture.jsx
+++ src/components/WebcamCapture.jsx
@@ -1,8 +1,9 @@
 import {useState, useEffect, useRef,  useCallback} from "react";
 import Webcam from "react-webcam";
+// import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 
-const WebcamCapture = ({getCurrentTime,stopWebcam}) => {
+const WebcamCapture = ({firebase, getCurrentTime,stopWebcam}) => {
     const webcamRef = useRef(null);
 
 
@@ -11,12 +12,30 @@ const WebcamCapture = ({getCurrentTime,stopWebcam}) => {
 
     const capture = useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
+        console.log({imageSrc})
         setImgSrc({
             imageSrc,
             time: getCurrentTime()
         });
-        stopWebcam()
+        // stopWebcam()
     }, [webcamRef, setImgSrc]);
+
+    const upload = () => {
+        // Create a root reference
+        console.log("Uploading")
+
+        var storageRef = firebase.storage().ref();
+        var mountainsRef = storageRef.child('mountains.jpg');
+
+
+            // 'file' comes from the Blob or File API
+        console.log("Uploading")
+        console.log(imgSrc.imageSrc)
+        mountainsRef.putString(imgSrc.imageSrc, 'data_url').then((snapshot) => {
+            console.log('Uploaded a blob or file!');
+        });
+
+    }
 
     return (
         <div className='webcam'>
@@ -27,6 +46,7 @@ const WebcamCapture = ({getCurrentTime,stopWebcam}) => {
             />
             <button onClick={capture}>Capture photo</button>
             <button onClick={stopWebcam}>Cancel</button>
+            <button onClick={upload}>Upload</button>
             {imgSrc && (
                 <>
                 <img
@@ -35,6 +55,7 @@ const WebcamCapture = ({getCurrentTime,stopWebcam}) => {
                     <p>{`${imgSrc.time.toDateString()} ${imgSrc.time.toTimeString()} ${imgSrc.time.getMilliseconds()}`}</p>
                 </>
             )}
+            {/*<p>{JSON.stringify(imgSrc)}</p>*/}
         </div>
     );
 };
