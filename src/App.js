7937e90433a9913a7db25b9a13ffbdc385fde9b8
davidrocks88 <D1a0v9i4dX1e0n9o4>
2021-04-18 00:03:19 -0400

    starting firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from "firebase";
+require('dotenv').config()
+
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+});
+
+const db = firebaseApp.firestore();
+
+export { db };
+
 function App() {
+
+  const click = () => {
+    // Add a new document in collection "cities"
+    db.collection("cities").doc("LA").set({
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA"
+    })
+        .then(function() {
+          console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+          console.error("Error writing document: ", error);
+        });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +50,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={click}>Click me!</button>
       </header>
     </div>
   );
