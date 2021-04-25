5bec65d36960593f6d4e3525c6a09f2b0486ff92
davidrocks88 <D1a0v9i4dX1e0n9o4>
2021-04-25 11:52:35 -0400

    got image preview working

--- src/components/CreateWatch.jsx
+++ src/components/CreateWatch.jsx
@@ -8,44 +8,47 @@ String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1)
 }
 
-// const Thumbnail = ({file}) => {
-//
-//
-//     const [loading, setLoading] = useState(false)
-//     const [thumb, setThumb] = useState()
-//
-//
-//     if (!file) { return null; }
-//
-//     if (loading) { return <p>loading...</p>; }
-//
-//     this.setState({ loading: true }, () => {
-//         let reader = new FileReader();
-//
-//         reader.onloadend = () => {
-//             this.setState({ loading: false, thumb: reader.result });
-//         };
-//
-//         reader.readAsDataURL(nextProps.file);
-//     });
-//
-//     return (<img src={thumb}
-//                  alt={file.name}
-//                  className="img-thumbnail mt-2"
-//                  height={200}
-//                  width={200} />);
-//
-// }
+const Thumbnail = ({file}) => {
+
+
+    const [loading, setLoading] = useState(false)
+    const [thumb, setThumb] = useState()
+
+
+    if (!file) { return null; }
+
+    if (loading) { return <p>loading...</p>; }
+
+    let reader = new FileReader();
+
+    reader.onloadend = () => {
+        setLoading(false)
+        setThumb(reader.result)
+    };
+
+    reader.onerror = error => console.log(error)
+    reader.onabort = abort => console.log(abort)
+
+    console.log(file)
+
+    reader.readAsDataURL(file);
+
+    return (<img src={thumb}
+                 alt={file.name}
+                 className="img-thumbnail mt-2"
+                 height={200}/>);
+
+}
 
 const CreateWatch = () => {
-    const textFieldsArr = ["name", "model", "year", "notes"]
-    const textFields = textFieldsArr.reduce((acc,curr)=> (acc[curr]='', acc),{});
+    const textFields = ["name", "model", "year", "notes"]
+
+    const [image, setImage] = useState()
 
 
     const formik = useFormik({
         initialValues: {
-            ...textFields,
-            file: null
+            ...textFields.reduce((acc,curr)=> (acc[curr]='', acc),{}),
         },
         onSubmit: (values) => {
             alert(JSON.stringify(values, null, 2));
@@ -57,9 +60,10 @@ const CreateWatch = () => {
             <h1>Create New Watch</h1>
 
             <form onSubmit={formik.handleSubmit}>
-                {textFieldsArr.map(fieldName =>
+                {textFields.map(fieldName =>
                     <TextField
                     fullWidth
+                    key={fieldName}
                     id={fieldName}
                     name={fieldName}
                     label={fieldName.capitalize()}
@@ -72,6 +76,24 @@ const CreateWatch = () => {
                 />)}
 
                 <div style={{marginBottom: '2em'}}/>
+
+                <div className="form-group">
+                    <label htmlFor="image">Image</label>
+                    <input
+                        id="image"
+                        name="image"
+                        type="file"
+                        onChange={event => {
+                            setImage(event.target.files[0])
+                            formik.setFieldValue('image', event.target.files[0])
+                        }}
+                    />
+                    <Thumbnail file={image}/>
+
+                </div>
+
+                <div style={{marginBottom: '2em'}}/>
+
 
                 <Button color="primary" variant="contained" fullWidth type="submit">
                     Submit
